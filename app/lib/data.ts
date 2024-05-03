fbd81acb3320a477fb223c05aa17fc3910a60c20
Suraj <surajutt@iitbhilai.ac.in>
2024-05-04 02:10:46 +0530

    Added dashboard content

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -69,7 +69,7 @@ export async function fetchCardData() {
       customerCountPromise,
       invoiceStatusPromise,
     ]);
-
+    console.log(data);
     const numberOfInvoices = Number(data[0].rows[0].count ?? '0');
     const numberOfCustomers = Number(data[1].rows[0].count ?? '0');
     const totalPaidInvoices = formatCurrency(data[2].rows[0].paid ?? '0');
